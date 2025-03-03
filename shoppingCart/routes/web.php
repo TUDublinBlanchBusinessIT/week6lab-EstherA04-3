14f707e7a6b9af3249aea2435f6426d12ed1b764
B00144653 Iyanuoluwa Akinlade <B00144653@mytudublin.ie>
2025-03-03 16:02:06 +0000

    got something working

--- shoppingCart/routes/web.php
+++ shoppingCart/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('oderdetails', App\Http\Controllers\oderdetailController::class);
+
+
+Route::resource('products', App\Http\Controllers\productController::class);
+
+
+Route::resource('scorders', App\Http\Controllers\scorderController::class);
+
+
+Route::get('product/displaygrid', 'App\Http\Controllers\productController@displaygrid')->name('products.displaygrid');
